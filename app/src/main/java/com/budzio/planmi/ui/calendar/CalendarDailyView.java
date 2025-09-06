b05a2ca4842367cc0cf4dcc5d74f2b092dff6273
budziot <budzio.zsm1@gmail.com>
2025-09-06 12:40:33 +0200

    Trying to do activity stuff

--- app/src/main/java/com/budzio/planmi/ui/calendar/CalendarDailyView.java
+++ app/src/main/java/com/budzio/planmi/ui/calendar/CalendarDailyView.java
@@ -1,6 +1,9 @@
 package com.budzio.planmi.ui.calendar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,16 @@ public class CalendarDailyView extends AppCompatActivity {
         setContentView(R.layout.activity_daily_view);
 
         chosenDate = LocalDate.now();
+
+        // Back button to start screen connection
+        ImageButton menuBackBtn = findViewById(R.id.calendar_back_btn);
+        menuBackBtn.setOnClickListener(this::goBackToMenu);
+    }
+
+    private void goBackToMenu(View view) {
+        Intent intent = new Intent(CalendarDailyView.this, view.getClass());
+        startActivity(intent);
+
+        overridePendingTransition(R.anim.slide_from_right, 0);
     }
 }
